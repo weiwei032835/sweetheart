45c6b0f56d4ac6570d808ad4e392f326dbe2487b
MSI\chen wei <wiwei032835@gmail.com>
2025-03-23 20:09:52 +0800

    加入聊天機器人

---
+++ js/common.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $("#chat-icon").click(function () {
+        $("#chat-container").fadeToggle();
+    });
+
+    $("#close-chat").click(function () {
+        $("#chat-container").fadeOut();
+    });
+});
